c4bd989bc2dc020c1a1b30562e21aa7a0cb26828
HyakkiOp <88389394+HyakkiOp@users.noreply.github.com>
2024-12-08 19:06:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, AUTO_DELETE_TIME, AUTO_DELETE_MSG, JOIN_REQUEST_ENABLE,FORCE_SUB_CHANNEL
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, AUTO_DELETE_TIME, AUTO_DELETE_MSG, JOIN_REQUEST_ENABLE, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2
 from helper_func import subscribed,decode, get_messages, delete_file
 from database.database import add_user, del_user, full_userbase, present_user
 
@@ -173,13 +173,13 @@ async def not_joined(client: Client, message: Message):
     else:
         ButtonUrl = client.invitelink
 
-    buttons = [
+        buttons = [
         [
-            InlineKeyboardButton(
-                "Join Channel",
-                url = ButtonUrl)
-        ]
+            InlineKeyboardButton(text="Channel 1", url=client.invitelink),
+            InlineKeyboardButton(text="Channel 2", url=client.invitelink2),
+        ],
     ]
+
 
     try:
         buttons.append(
